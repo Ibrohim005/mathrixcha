11e5814b7d86e1718180fde0c2b2281386817854
imron <karombekh@gmail.com>
2021-09-07 22:15:33 +0500

    pull: fixed version

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,5 +1,9 @@
 <template>
-    
+    <p class="text-lg font-semibold">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+    </p>
 </template>
 
 <script>
